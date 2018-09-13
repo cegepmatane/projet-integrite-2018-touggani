cc603391eea56d8291e8cab2b755bcb7472a208a
touggani <35734200+touggani@users.noreply.github.com>
2018-09-13 02:00:37 -0400

    Add files via upload

--- src/donnee/VainqueurDAO.java
+++ src/donnee/VainqueurDAO.java
@@ -1,5 +1,7 @@
 package donnee;
 import java.util.ArrayList;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import modele.Vainqueur;
 
@@ -15,6 +17,22 @@ public class VainqueurDAO {
 	}
 	public List<Vainqueur> listerVainqueur()
 	{
+		String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+		String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/bergerie";
+		String BASEDEDONNEES_USAGER = "postgres";
+		String BASEDEDONNEES_MOTDEPASSE = "test";
+		
+		try {
+			Class.forName(BASEDEDONNEES_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return this.simulerListerVainqueur();
 	}
 }
